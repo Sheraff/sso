fa5a826699db476c91b6367c079a382f4c7633cf
Sheraff <fpellet@ensc.fr>
2026-01-06 00:00:02 +0100

    try tx2 again

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ import { webServer } from "./src/web/server.ts"
 import { createSessionManager } from "./src/sessions/sessions.ts"
 import { createInvitationManager } from "./src/invitations/invitations.ts"
 import { ipcServer } from "./src/ipc/server.ts"
+import tx2 from 'tx2'
 
 const db = new Database(process.env.DATABASE_PATH)
 {
@@ -17,6 +18,11 @@ const invitationManager = createInvitationManager(db)
 
 const ipc = ipcServer(sessionManager, invitationManager)
 const web = webServer(sessionManager, invitationManager)
+
+tx2.action('invite', (reply) => {
+	const code = invitationManager.generateInvitationCode()
+	reply({ answer: code })
+})
 
 process.on("SIGINT", async () => {
 	console.log("\nSIGINT received, shutting down...")
