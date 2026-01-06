8967f1f0f42b2544826ac276950109c6edc76405
Sheraff <fpellet@ensc.fr>
2026-01-06 15:14:13 +0100

    more fun code gen

--- server/src/invitations/generateCode.ts
+++ server/src/invitations/generateCode.ts
@@ -1,18 +1,21 @@
 import { readFileSync } from 'node:fs'
 
-const CODE_LENGTH = 3
+const CODE_LENGTH = 22
 
 export function generateCode(existingCodes: string[]) {
-	const WORDS = readFileSync(new URL('./5-letter-words.txt', import.meta.url), 'utf-8').split("\n")
+	const WORDS = readFileSync(new URL('./5-6-letter-words.txt', import.meta.url), 'utf-8').split("\n")
 	const count = WORDS.length
 	while (true) {
-		const indices: number[] = []
-		while (indices.length < CODE_LENGTH) {
+		const words = new Array<string>()
+		let length = 0
+		while (length < CODE_LENGTH) {
 			const index = Math.floor(Math.random() * count)
-			if (indices.includes(index)) continue
-			indices.push(index)
+			const word = WORDS[index]
+			words.push(word)
+			if (length > 0) length++ // space
+			length += word.length
 		}
-		const code = indices.map((i) => WORDS[i]).join(" ")
+		const code = words.join(' ')
 		if (existingCodes.includes(code)) continue
 		return code
 	}
